085ba09f9376ccae19ed62d1ceb6b3633d24fc78
Anita Caron <anitacaron@users.noreply.github.com>
2023-03-06 09:25:27 -0300

    add mapping_id to other field

--- src/database/sparql_implementation.py
+++ src/database/sparql_implementation.py
@@ -19,6 +19,8 @@ from sssom_schema import SSSOM
 from ..models import Mapping, MappingSet
 from ..utils import parse_fields_type
 
+import json
+
 class SparqlImpl(SparqlImplementation):
   def __post_init__(self, schema_view):
     super(SparqlImplementation, self).__post_init__()
@@ -104,12 +106,23 @@ class SparqlImpl(SparqlImplementation):
         out.append(v["value"])
     return out
 
+  def add_id(self, row):
+    print(row['_x'])
+    if row.get('other'):
+      other = json.loads(row["other"])
+      other["mapping_id"] = row["_x"]
+      row["other"] = json.dumps(other)
+    else:
+      row["other"] = json.dumps({"mapping_id": row["_x"]})
+    row.pop("_x")
+  
+    return row
+  
   def get_sssom_mappings_by_field(self, field: str, value: str) -> Iterable[Mapping]:
     default_query = self.default_query(type=Mapping.class_class_uri, slots=self.schema_view.mapping_slots.copy(), field=field, value=value)
     bindings = self._query(default_query)
     for row in bindings:
-      r = self.transform_result(row)
-      r.pop("_x")
+      r = self.add_id(self.transform_result(row))
       r[f"{field}"] = value
       m = create_sssom_mapping(**r)
       if m is not None:
@@ -128,8 +141,7 @@ class SparqlImpl(SparqlImplementation):
     default_query = self.add_filters(self.default_query(Mapping.class_class_uri, self.schema_view.mapping_slots.copy()), filter)
     bindings = self._query(default_query)
     for row in bindings:
-      r = self.transform_result(row)
-      r.pop("_x")
+      r = self.add_id(self.transform_result(row))
       m = create_sssom_mapping(**r)
       if m is not None:
         yield m
@@ -163,8 +175,7 @@ class SparqlImpl(SparqlImplementation):
     default_query = self.default_query(Mapping.class_class_uri, slots=self.schema_view.mapping_slots.copy()+["mapping_set"], field="mapping_set", value=f'{SSSOM}{id}', inverse=True)
     bindings = self._query(default_query)
     for row in bindings:
-      r = self.transform_result(row)
-      r.pop("_x")
+      r = self.add_id(self.transform_result(row))
       m = create_sssom_mapping(**r)
       if m is not None:
         yield m
