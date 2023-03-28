25f8695b51bd0ebf86019887eaff249ebd30f571
Anita Caron <anitac@ebi.ac.uk>
2023-03-28 08:00:01 -0300

    Add facets to each page (#53)
    * create FacetInfo and add to Page model
    
    * add facets in the page result
    
    * change facet name to `predicate_id`
    
    * use countby in facets
    
    ---------
    
    Co-authored-by: Anita Caron <anitacaron@users.noreply.github.com>

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,13 @@
 
 from typing import Iterable, TypeVar, Union, List
 import itertools
-import functools
 import math
+from toolz.recipes import countby
+from toolz.itertoolz import count
 
 from fastapi import Request
 
-from .models import Page, PaginationInfo
+from .models import Page, PaginationInfo, FacetInfo
 
 
 def _replace_page_param(request: Request, new_page: Union[int, None]) -> Union[str, None]:
@@ -24,8 +25,8 @@ def paginate(iterable: Iterable[T], page: int, limit: int, request: Request) -> 
     prev_page = None
     next_page = None
     data = []
-    iter_data, iter_total = itertools.tee(iterable)
-    total_items = functools.reduce(lambda prev, curr: prev + 1, iter_total, 0)
+    iter_data, iter_total, iter_facets = itertools.tee(iterable, 3)
+    total_items = count(iter_total)
     total_pages = math.ceil(total_items / limit)
     for idx, item in enumerate(iter_data):
         if idx == start - 1:
@@ -44,6 +45,15 @@ def paginate(iterable: Iterable[T], page: int, limit: int, request: Request) -> 
             total_items=total_items,
             total_pages=total_pages
         ),
+        facets=_create_facets(iter_facets)
+    )
+
+def _create_facets(data: Iterable[T]) -> FacetInfo:
+    iter_mj, iter_pred = itertools.tee(data)
+    
+    return FacetInfo(
+        mapping_justification=countby(lambda d: d["mapping_justification"], iter_mj),
+        predicate_id=countby(lambda d: d["predicate_id"], iter_pred),
     )
 
 def parser_filter(datamodel: T, filter: Union[List[str], None] = None) -> Union[List[dict], None]:
