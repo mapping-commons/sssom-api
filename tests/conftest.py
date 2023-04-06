09bca7b9cd262aab737f67b2953333c9391dce94
Anita Caron <anitacaron@users.noreply.github.com>
2023-04-06 16:55:11 +0100

    fix tests files

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,15 @@
-from unittest.mock import Mock
-
 import pytest
+from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
 from src.main import create_app
 
-@pytest.fixture
-def app():
-    return create_app()
 
 @pytest.fixture
-def client(app) -> TestClient:
+def app() -> FastAPI:
+    return create_app()
+
+
+@pytest.fixture
+def client(app: FastAPI) -> TestClient:
     return TestClient(app)
--- tests/test_routers_mappings.py
+++ tests/test_routers_mappings.py
@@ -1,11 +1,14 @@
 from fastapi.testclient import TestClient
 
+
 def test_get_single_mapping(client: TestClient):
     id = "8c835ed637ab5d11a29d088080f10114"
     response = client.get(f"/mappings/{id}")
     assert response.status_code == 200
     body = response.json()
     assert body == {
-        ""
+        "subject_id": "http://purl.obolibrary.org/obo/MP_0001289",
+        "predicate_id": "http://www.w3.org/2004/02/skos/core#closeMatch",
+        "object_id": "http://purl.obolibrary.org/obo/HP_0007968",
+        "mapping_justification": "https://w3id.org/semapv/LexicalMatching",
     }
-    
