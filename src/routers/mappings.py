d1c495274565551db5dc07983cc358d46d06d8a2
Anita Caron <anitac@ebi.ac.uk>
2023-04-21 17:59:10 +0100

    Add multiple value fields to mappings (#64)
    * add mappings endpoint to ui
    
    * formatting
    
    ---------
    
    Co-authored-by: Anita Caron <anitacaron@users.noreply.github.com>

--- src/routers/mappings.py
+++ src/routers/mappings.py
@@ -6,6 +6,7 @@ from sssom_schema import Mapping
 from ..database.sparql_implementation import (
     SparqlImpl,
     get_mapping_by_id,
+    get_mappings_by_filter_ui,
     get_mappings_field,
     get_mappings_query,
     get_ui_mapping_by_id,
@@ -58,3 +59,17 @@ router_ui = APIRouter(prefix="/mappings", tags=["mappings"])
 @router_ui.get("/{id}", summary="Get mapping by id")
 def mapping_by_id_ui(id: str, sparqlImpl: SparqlImpl = Depends(get_sparql_implementation)):
     return get_ui_mapping_by_id(sparqlImpl, id)
+
+
+@router_ui.get("/", summary="Get mappings with optional filters")
+def mappings_ui(
+    sparqlImpl: SparqlImpl = Depends(get_sparql_implementation),
+    pagination: PaginationParams = Depends(),
+    filter: Union[List[str], None] = Query(default=None),
+):
+    filter_parsed = parser_filter(Mapping, filter)
+    if filter_parsed is None:
+        raise HTTPException(status_code=302, detail="Not valid filter")
+    else:
+        results = get_mappings_by_filter_ui(sparqlImpl, filter_parsed)
+        return paginate(results, **pagination.dict())
